74055d524c9ddab137603cfc33cd3d068e2f0de7
Lauryn B <lauryn.m.branham@icloud.com>
2024-04-24 16:04:27 -0500

    WIP

--- bot.py
+++ bot.py
@@ -1,9 +1,23 @@
 
 import discord
+import os
 from discord.ext import commands
 import aiosqlite
 import random
 from datetime import datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
 
 bot = commands.Bot(command_prefix='!')
 
